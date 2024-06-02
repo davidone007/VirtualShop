e464fec3c63d9a66e6ab0cfcbd1192ce3a837051
davidone007 <davidecazaran@gmail.com>
2024-06-02 16:36:12 -0500

    corrigiendo errores

--- src/client/controllers/productsController.js
+++ src/client/controllers/productsController.js
@@ -1,22 +1,22 @@
-document.getElementById('productForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const formData = new FormData(this);
+document.getElementById('productForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const formData = new FormData(this);
 
-    fetch('http://localhost:3000/products', {
-      method: 'POST',
-      body: formData,
-    })
+  fetch('http://localhost:3000/products', {
+    method: 'POST',
+    body: formData,
+  })
     .then(response => response.json())
     .then(result => {
       console.log('Success:', result);
-      alert('Producto agregado exitosamente!');
+      alert('Product successfully added!');
     })
     .catch(error => {
       console.error('Error:', error);
-      alert('Hubo un error al agregar el producto.');
+      alert('There was an error adding the product!');
     });
-  });
+});
 
-  document.getElementById('logout').addEventListener('click', function() {
-    window.location.href = 'html/login.html'; 
+document.getElementById('logout').addEventListener('click', function () {
+  window.location.href = 'html/login.html';
 });
